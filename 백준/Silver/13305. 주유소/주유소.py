b508e7247420c9bc073ed5a86a7b1417fd14ef2d
Hwarang Park <asdf78964@naver.com>
2025-11-27 14:45:22 +0900

    [Silver III] Title: 주유소, Time: 104 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,24 +1,15 @@
-city_count = int(input())
+n = int(input())
 
-road_km = list(map(int, input().split()))
-oil_price = list(map(int, input().split()))
+km = list(map(int, input().split()))
+price = list(map(int, input().split()))
 
-money = 0
-price = 0
+start_price = price[0]
+total_money = 0
 
-for i in range(len(road_km)):
-    if oil_price[i] == min(oil_price[:len(road_km)]):
-        money += sum(list(map(lambda x : x * oil_price[i], road_km[i:])))
-        break
-
-    elif oil_price[i] <= oil_price[i+1]:
-        price = oil_price[i]
-        money += oil_price[i] * price
+for i in range(n-1):
+    if price[i] < start_price:
+        start_price = price[i]
     
-    elif i != 0 and oil_price[i] > oil_price[i-1]:
-        money += oil_price[i] * price
-
-    else:
-        money += oil_price[i] * road_km[i]        
-
-print(money)
+    total_money += km[i] * start_price
+    
+print(total_money)
